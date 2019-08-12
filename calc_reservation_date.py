ab193fe6a1ccf0fdeb30296fe8b0c9ee34a9a78c
Yusuke Konishi <ysk.kns@gmail.com>
2019-08-12 15:38:01 +0900

    convert string to datetime/timedelta objects

--- calc_reservation_date.py
+++ calc_reservation_date.py
@@ -3,14 +3,23 @@
 import sys
 import datetime
 
-# take target date and duration from command line
+# take target date and duration days from command line
 if len(sys.argv) < 3:
     sys.exit('''
         Usage: calc_reservation_date.py <YYYY/MM/DD of target date> <duration days>
-           Ex: calc_reservation_date.py 2019/12/24 59
+           Ex: calc_reservation_date.py 2019/12/24 90
     ''')
 
 target_date = sys.argv[1]
 duration_days = sys.argv[2]
 
-# TODO: translate string to datetime/timedelta objects
+# convert string to datetime/timedelta objects
+datetime_target_date = datetime.datetime.strptime(target_date, '%Y/%m/%d')
+timedelta_duration_days = datetime.timedelta(days=int(duration_days))
+
+datetime_reservation_date = datetime_target_date - timedelta_duration_days
+reservation_date = datetime_reservation_date.strftime('%Y/%m/%d (%a)')
+
+print('''
+        Make your reservation on {} !!!!
+'''.format(reservation_date))
